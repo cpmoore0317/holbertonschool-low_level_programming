82c4745bb27c8b4658e5114d4917ae97fa4a99f0
cpmoore0317 <cpmoore0317@gmail.com>
2023-05-25 06:49:59 -0700

    printeff

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    prinf("holbertonschool-low_level_programming\n");	
+    return (0);
+}
