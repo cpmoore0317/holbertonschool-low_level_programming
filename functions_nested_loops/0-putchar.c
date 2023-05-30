3d1a69bcd43791292f0f5a886281bc887c247b57
cpmoore0317 <kingpkayy@gmail.com>
2023-05-30 14:39:57 -0700

    putchar1

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - uses putchar to display _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+int x;
+
+char a[] = "_putchar";
+
+	for (x = 0 ; x < 9 ; x++)
+	{
+	putchar(a[x]);
+	}
+	putchar('\n');
+	return (0);
+}
