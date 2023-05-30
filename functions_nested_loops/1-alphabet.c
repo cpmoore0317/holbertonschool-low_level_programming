557ef5b12d10c7cb96f15eb293fc2c525548b219
cpmoore0317 <kingpkayy@gmail.com>
2023-05-30 16:40:13 -0700

    voidprintalphabet

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -2,22 +2,20 @@
 #include "main.h"
 
 /**
- * main - Uses the _putchar function to print the
+ * print_alphabet - Uses the _putchar function to print the
  * alphabet, in lowercase, followed by a new line.
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+void print_alphabet(void)
 
 {
-
 	char alphabet = 'a';
 
 	while (alphabet <= 'z')
 	{
-		putchar(alphabet);
+		_putchar(alphabet);
 		alphabet++;
 	}
-	putchar('\n');
-	return (0);
+	_putchar('\n');
 }
