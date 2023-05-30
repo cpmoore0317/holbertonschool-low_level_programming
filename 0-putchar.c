45092719800fd5569e495f4be11617e563348241
cpmoore0317 <kingpkayy@gmail.com>
2023-05-30 09:58:26 -0700

    putchar

---
+++ 0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - uses putchar to display _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+int x;
+
+char a[] = "_putchar";
+
+	for (x = 0 ; x < 9 ; x++)
+	{
+	putchar(a[x]);
+	}
+	return (0);
+}
