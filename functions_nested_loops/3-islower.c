90551ac507b10475595e2d2272b3ae1ffcb8822e
cpmoore0317 <kingpkayy@gmail.com>
2023-05-30 21:37:04 -0700

    islowerwhile

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -3,6 +3,7 @@
 
 /**
  * _islower - checks for lowercase number
+ * @c: character to be compared
  *
  * Return: Always 0
  */
@@ -11,10 +12,12 @@ int _islower(int c)
 	int a = 97;
 
 	while (a <= 122)
-		a++;
-	return (0);
+	{
 		if (c == a)
 		{
 			return (1);
 		}
+	a++;
+	}
+	return (0);
 }
