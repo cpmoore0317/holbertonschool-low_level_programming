3e588e884727c3a5777acd989bac1365d9b16f81
cpmoore0317 <kingpkayy@gmail.com>
2023-05-30 19:47:40 -0700

    islowerrr

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase number
+ *
+ * Return: Always 0
+ */
+int _islower(int c)
+{
+	int a = 97;
+
+	while (a <= 122)
+		a++;
+	return (0);
+		if (c == a)
+		{
+			return (1);
+		}
+}
