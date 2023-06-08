778c9f403dc7e93f4ccab356df9fd2d67a46dee9
cpmoore0317 <kingpkayy@gmail.com>
2023-06-08 11:01:57 -0700

    capstring

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,2 +1,27 @@
 #include <stdio.h>
 #include "main.h"
+#include <ctype.h>
+/**
+ * cap_string - Capitalizes all words of a string
+ * @str: String to be altered
+ *
+ * Return: New string
+ */
+char *cap_string(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
+			str[i] == ',' || str[i] == ';' || str[i] == '.' ||
+			str[i] == '!' || str[i] == '?' || str[i] == '"' ||
+			str[i] == '(' || str[i] == ')' || str[i] == '{' ||
+			str[i] == '}')
+		{
+			continue;
+		}
+		str[i] = toupper(str[i]);
+	}
+	return (str);
+}
