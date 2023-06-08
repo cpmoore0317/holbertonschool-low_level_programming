7b96e38802f8824845dcb2ab64cc3a30cfb2dc0e
cpmoore0317 <kingpkayy@gmail.com>
2023-06-08 11:15:49 -0700

    leet

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -1,2 +1,27 @@
 #include <stdio.h>
 #include "main.h"
+#include <ctype.h>
+/**
+ * leet - Encodes a string into 1337
+ * @str: String to be altered
+ *
+ * Return: Altered string
+ */
+char *leet(char *str)
+{
+	long unsigned int i, j;
+	char letters[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	unsigned char replacements[] = {'4', '3', '0', '7', '1'};
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j < sizeof(letters) / sizeof(letters[0]); j++)
+		{
+			if (str[i] == letters[j])
+			{
+				str[i] = replacements[j];
+			}
+		}
+	}
+	return (str);
+}
