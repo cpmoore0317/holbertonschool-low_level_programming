2b96055c48cb3ece6d54e4cddcc4c292380d1626
cpmoore0317 <kingpkayy@gmail.com>
2023-06-08 10:35:06 -0700

    compare

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,2 +1,22 @@
 #include <stdio.h>
 #include "main.h"
+#include <string.h>
+/**
+ * _strcmp - Compares two strings
+ * @s1: First string to be compared
+ * @s2: Second string to be compared
+ *
+ * Return: 0 if strings are equal, or return the
+ * difference between two characters
+ */
+int _strcmp(char *s1, char *s2)
+{
+	for (; *s1 != '\0' && *s2 != '\0'; s1++, s2++)
+	{
+		if (*s1 != *s2)
+		{
+			return (*s1 - *s2);
+		}
+	}
+	return (*s1 - *s2);
+}
