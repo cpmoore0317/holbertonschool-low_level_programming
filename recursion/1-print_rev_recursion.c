70d6585952df268ec4db733479d0557c23ed0e1b
cpmoore0317 <kingpkayy@gmail.com>
2023-06-15 16:04:47 -0700

    printrevrecursion

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Prints a string in reverse
+ * @s: String
+ *
+ * Return: None
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	printf("%c", *s);
+}
