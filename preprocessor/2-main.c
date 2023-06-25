b98026ecf96af6ff047717428a0e0628fab6c6cb
cpmoore0317 <kingpkayy@gmail.com>
2023-06-25 11:31:25 -0700

    preprocessor

---
+++ preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints the filename I was compiled from
+ *
+ * Return: Always returns 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
