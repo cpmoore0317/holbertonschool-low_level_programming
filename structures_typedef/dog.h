7deca487e03e84c3ad76d2eedc2c9f01c729e84e
cpmoore0317 <kingpkayy@gmail.com>
2023-06-29 15:50:31 -0700

    dogh2

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,18 @@
-struct dog {
-  char *name;
-  float age;
-  char *owner;
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Defines structure dog
+ * @name: The given name for the dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
+ *
+ * description - Holds the three pieces of data on the dog
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
 };
