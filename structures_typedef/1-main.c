432939d562da0874adcbe206492ee3cd5e058f97
cpmoore0317 <kingpkayy@gmail.com>
2023-06-29 16:10:38 -0700

    initdog2

--- structures_typedef/1-main.c
+++ structures_typedef/1-main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "dog.h"
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
 /**
  * main - check the code
  *
@@ -9,8 +11,8 @@
 int main(void)
 {
     struct dog my_dog;
-
     init_dog(&my_dog, "Poppy", 3.5, "Bob");
+
     printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
     return (0);
 }
