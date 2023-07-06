8b39f910d8ed2f9002c81c71273e292c201c6f7a
“cpmoore0317” <kingpkayy@gmail.com>
2023-07-05 18:35:23 -0700

    say my name say my nameee

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints its name with a newline
+ * @argc: Number of arguments typed
+ * @argv: Array pointing to arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	argc = 0;
+
+	printf("%s\n", argv[argc]);
+	return (0);
+}
