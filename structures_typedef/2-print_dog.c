f865ec29abb2e0c6ef68abb147422bb53b1bfa2d
“cpmoore0317” <kingpkayy@gmail.com>
2023-07-05 20:28:27 -0700

    making sure the printf function follows the correct loop

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -12,14 +12,19 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
+
 	if (d->name == NULL)
 		d->name = "(nil)";
+	else
+		printf("Name: %s\n", d->name);
+
 	if (d->age <= 0)
 		printf("Age: (nil)\n");
+	else
+		printf("Age: %.6f\n", d->age);
+
 	if (d->owner == NULL)
 		d->owner = "(nil)";
-
-	printf("Name: %s\n", d->name);
-	printf("Age: %.6f\n", d->age);
-	printf("Owner: %s\n", d->owner);
+	else
+		printf("Owner: %s\n", d->owner);
 }
