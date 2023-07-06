bb8bff843ae922a4183e85895a4f151ecc47990e
“cpmoore0317” <kingpkayy@gmail.com>
2023-07-05 20:52:16 -0700

    recursion

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -11,16 +11,10 @@
  */
 int _pow_recursion(int x, int y)
 {
+	if (y < 0)
+		return (-1);
 	if (y == 0)
-	{
 		return (1);
-	}
-	else if (y % 2 == 0)
-	{
-		return (_pow_recursion(x, y / 2) * _pow_recursion(x, y / 2));
-	}
 	else
-	{
 		return (x * _pow_recursion(x, y - 1));
-	}
-}
+}	
