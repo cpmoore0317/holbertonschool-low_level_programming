64b1afe26e73dde1fe74a58ea4ff8e6e42f98606
“cpmoore0317” <kingpkayy@gmail.com>
2023-07-05 17:47:00 -0700

    fixing separator pointer and comma for argument

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -15,14 +15,14 @@ void prints_strings(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list list;
 
-	if (separator == NULL || *separator = 0)
+	if (separator == NULL || *separator == 0)
 		sep = "";
 	else
 		sep = (char *) separator;
 	va_start(list, n);
 
 	if (n > 0)
-		printf("%s". va_arg(list, char *));
+		printf("%s", va_arg(list, char *));
 	for (i = 1; i < n; i++)
 	{
 		ptr = va_arg(list, char *);
