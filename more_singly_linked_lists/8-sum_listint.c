4f9cf96de88940792e18f28c87f868695239a1b8
“cpmoore0317” <kingpkayy@gmail.com>
2023-07-15 14:45:05 -0700

    sumlistsinit

--- more_singly_linked_lists/8-sum_listint.c
+++ more_singly_linked_lists/8-sum_listint.c
@@ -4,3 +4,19 @@
  * @head: head
  * Return: 0
  */
+int sum_listint(listint_t *head)
+{
+	int sum;
+	listint_t *current;
+
+	sum = 0;
+	current = head;
+	
+	while (current != NULL)
+	{
+		sum += current->n;
+		current = current->next;
+	}
+
+	return (sum);
+}
