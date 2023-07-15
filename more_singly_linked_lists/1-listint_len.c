a8cd0e89ef93f4e73c02f69c5a73a05850da429d
“cpmoore0317” <kingpkayy@gmail.com>
2023-07-15 11:49:33 -0700

    creating desc

--- more_singly_linked_lists/1-listint_len.c
+++ more_singly_linked_lists/1-listint_len.c
@@ -1,3 +1,11 @@
+
+#include "lists.h"
+/**
+* listint_len - a function that returns the number
+* of elements in a linked listint_t list
+* @h: input info
+* Return: 0
+*/
 size_t print_listint(const listint_t *h)
 {
 	size_t n = 0;
