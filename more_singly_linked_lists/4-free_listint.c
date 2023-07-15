41f20c80657fa0d3e46f24571a9d38333b973b3b
“cpmoore0317” <kingpkayy@gmail.com>
2023-07-15 14:22:21 -0700

    frelistinpt1

---
+++ more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_listint - a function that frees a listint_t list.
+ * @head: struct
+ * Return: free
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *current;
+
+	if (head == NULL)
+		return;
+
+	current = head;
+
+	while (current != NULL)
+	{
+		listint_t *next = current->next;
+		free(current);
+		current = next;
+	}
+}
