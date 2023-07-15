8ab20ed3a9ec7d2789bfbf668eef6f3fbce3f929
“cpmoore0317” <kingpkayy@gmail.com>
2023-07-15 15:10:23 -0700

    fixing 9 pt3

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -20,7 +20,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (new_node == NULL)
 		return;
 
-	new_node->n = data;
+	new_node->n = n;
 	new_node->next = NULL;
 
 	if (index == 0)
@@ -39,7 +39,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		current = current->next;
 	}
 
+	if (current == NULL)
+		return (NULL);
+
 	new_node->next = current->next;
 	current->next = new_node;
+
+	return new_node;
 }
 
