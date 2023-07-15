2abaecb5d274587f2352d90417652c5336f886ff
“cpmoore0317” <kingpkayy@gmail.com>
2023-07-15 15:13:47 -0700

    deletenode

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -9,3 +9,37 @@
  *
  * Return: 1 if successful, -1 otherwise
  */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+        listint_t *current;
+        listint_t *previous;
+        unsigned int i;
+
+        if (head == NULL || *head == NULL)
+                return -1;
+
+        if (index == 0)
+        {
+                current = *head;
+                *head = current->next;
+                free(current);
+                return 1;
+        }
+
+        current = *head;
+        for (i = 0; i < index - 1; i++)
+        {
+                if (current == NULL)
+                        return -1;
+                current = current->next;
+        }
+
+        if (current == NULL)
+                return -1;
+
+        previous = current->next;
+        current->next = previous->next;
+        free(previous);
+
+        return 1;
+}
