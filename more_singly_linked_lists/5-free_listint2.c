6afe8b21cfcf5b5f047f796b632ab548565c9b03
“cpmoore0317” <kingpkayy@gmail.com>
2023-07-15 14:28:55 -0700

    freelistsint2

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -4,3 +4,20 @@
  * @head: head
  * Return: 0
  */
+void free_listint2(listint_t **head)
+{
+	listint_t *current;
+
+	if (head == NULL)
+		return;
+
+	current = *head;
+	while (current != NULL)
+	{
+		listint_t *next = current->next;
+		free(current);
+		current = next;
+	}
+
+	*head = NULL;
+}
