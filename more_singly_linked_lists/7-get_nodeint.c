bf542ebb4a0ec9e4e92e67da65ecea6588e5c233
“cpmoore0317” <kingpkayy@gmail.com>
2023-07-15 14:38:07 -0700

    getnodeintpt1

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -5,3 +5,21 @@
  * @index: index
  * Return: head
  */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *current;
+	unsigned int i;
+
+	if (head == NULL)
+		return (NULL);
+
+	current = head;
+	for (i = 0; i < index; i++)
+	{
+		if (current == NULL)
+			return (NULL);
+		current = current->next;
+	}
+
+	return (current);
+}
