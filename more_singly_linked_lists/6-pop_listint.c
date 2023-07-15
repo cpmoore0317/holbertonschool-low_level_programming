1dce563866dde71fed7a302b50e3cb245cc6f1ce
“cpmoore0317” <kingpkayy@gmail.com>
2023-07-15 14:34:36 -0700

    poplist

--- more_singly_linked_lists/6-pop_listint.c
+++ more_singly_linked_lists/6-pop_listint.c
@@ -4,3 +4,18 @@
  * @head: head
  * Return: 0
  */
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int n;
+
+	if (head == NULL || *head == NULL)
+		return (0);
+	
+	temp = *head;
+	n = temp->n;
+	*head = temp->next;
+	free(temp);
+
+	return (n);
+}
