cee8a05df6ffeaa70332ace122a9ea91f5e35d40
“cpmoore0317” <kingpkayy@gmail.com>
2023-07-17 08:49:03 -0700

    printbinarypt2

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -9,15 +9,18 @@
 void print_binary(unsigned long int n)
 {
 	int i;
+	unsigned long int mask = 1UL << 63;
 
-	for (i = 63; i >= 0; i--)
+	for (i = 0; i < 64; i++)
 	{
-		if ((n >> i) & 1) {
+		if (n & mask)
+		{
 			_putchar('1');
 		}
 		else
 		{
 			_putchar('0');
 		}
+		mask >>= 1;
 	}
 }
