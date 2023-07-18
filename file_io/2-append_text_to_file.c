7d3aad388f6561b630e629ee012d5af6bfb3e926
“cpmoore0317” <kingpkayy@gmail.com>
2023-07-17 18:16:35 -0700

    append text to file pt1

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -3,3 +3,44 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * append_text_to_file - appends text at the end of a file
+ * @filename: name for file
+ * @text_content: text to put into file
+ * Return: 1 on success, -1 on failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	if (text_content == NULL)
+	{
+		FILE *fp = fopen(filename, "r");
+		if (fp == NULL)
+		{
+			return (-1);
+		}
+		else
+		{
+			fclose(fp);
+			return (1);
+		}
+	}
+
+	FILE *fp = fopen(filename, "a");
+	if (fp == NULL)
+	{
+		return (-1);
+	}
+
+	fputs(text_content, fp);
+
+	fclose(fp);
+
+	return (1);
+}
