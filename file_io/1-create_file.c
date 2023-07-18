db918615d06a952f3bc6d58a148a7edd9ef251a0
“cpmoore0317” <kingpkayy@gmail.com>
2023-07-17 18:09:54 -0700

    changing decleration of file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -11,6 +11,8 @@
  */
 int create_file(const char *filename, char *text_content)
 {
+	FILE *fp;
+
 	if (filename == NULL)
 	{
 		return (-1);
@@ -21,7 +23,6 @@ int create_file(const char *filename, char *text_content)
 		text_content = "";
 	}
 
-	FILE *fp;
 	if ((fp = fopen(filename, "w")) == NULL)
 	{
 		return (-1);
