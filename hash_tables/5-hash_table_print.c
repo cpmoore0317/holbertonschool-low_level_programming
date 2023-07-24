144ef3f318a019b568674936ea4e949ed6637e67
“cpmoore0317” <kingpkayy@gmail.com>
2023-07-24 09:32:47 -0700

    hash table print 2

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -4,3 +4,23 @@
 * @ht: hash tables
 * Return: a dictionary with the format of the hash table
 */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned int i;
+	hash_node_t *node;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node)
+		{
+			printf("'%s': '%s', ", node->key, node->value);
+			node = node->next;
+		}
+	}
+	printf("}\n");
+}
