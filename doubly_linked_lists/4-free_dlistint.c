7274d4d9665a234fe6079ff8bbb61d0d575f93c2
“cpmoore0317” <kingpkayy@gmail.com>
2023-07-24 08:51:20 -0700

    free dlistint 2

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * free_dlistint - frees list
+ * @head: head of node
+ * Return: free'd list
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *current;
+
+	while (head != NULL) {
+		current = head;
+		head = head->next;
+		free(current);
+	}
+}
