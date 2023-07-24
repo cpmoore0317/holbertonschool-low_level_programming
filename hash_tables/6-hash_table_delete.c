10bf88ff33748ac5a45c9180d36b6605b98bddb7
“cpmoore0317” <kingpkayy@gmail.com>
2023-07-24 09:34:50 -0700

    hash table delete 5

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -4,3 +4,26 @@
 * @ht: the hash table
 * Return: the hash table deleted
 */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned int i;
+	hash_node_t *node, *tmp;
+
+	if (ht == NULL)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node)
+		{
+			tmp = node;
+			node = node->next;
+			free(tmp->key);
+			free(tmp->value);
+		}
+	}
+
+	free(ht->array);
+	free(ht);
+}
