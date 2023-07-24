b381f8d638bf72c68721bce4a1d4ea0abc0bf595
“cpmoore0317” <kingpkayy@gmail.com>
2023-07-24 09:06:17 -0700

    insert nodeint 4

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+/**
+ * insert_dnodeint_at_index - inserts node at index
+ * @h: head of node
+ * @idx: index to insert node
+ * @n: data for new node
+ * Return: list with inserted node
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new_node = NULL;
+	dlistint_t *current = *h;
+	unsigned int i;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+	
+	new_node->n = n;
+	new_node->next = NULL;
+	new_node->prev = NULL;
+	
+	if (idx == 0)
+	{
+		new_node->next = *h;
+		if (*h != NULL)
+			(*h)->prev = new_node;
+		*h = new_node;
+		return (new_node);
+	}
+
+	for (i = 0; i < idx - 1 && current != NULL; i++)
+		current = current->next;
+
+	if (current == NULL)
+		return (NULL);
+
+	new_node->next = current->next;
+	if (current->next != NULL)
+		current->next->prev = new_node;
+	new_node->prev = current;
+	current->next = new_node;
+
+	return (new_node);
+}
