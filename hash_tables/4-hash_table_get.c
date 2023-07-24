b5efd3d99767cbdc2ef42c8aa4754dbe3c175070
“cpmoore0317” <kingpkayy@gmail.com>
2023-07-24 09:30:51 -0700

    hash table get 4

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -5,3 +5,19 @@
 * @key: key
 * Return: an element
 */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *node;
+
+	index = key_index((unsigned char *)key, ht->size);
+	node = ht->array[index];
+	while (node)
+	{
+		if (strcmp(node->key, key) == 0)
+			return (node->value);
+		node = node->next;
+	}
+
+	return (NULL);
+}
