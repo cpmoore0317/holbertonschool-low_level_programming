1d1ff1e7a1fce15bdff51d32ca624785faf1ca79
“cpmoore0317” <kingpkayy@gmail.com>
2023-08-19 11:51:21 -0700

    Adding necessary libraries to the header file

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -1,7 +1,9 @@
-#ifndef
-#define
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
 /* Libraries */
 #include <stdio.h>
+#include <stddef.h>
 
 /* Prototypes */
 int linear_search(int *array, size_t size, int value);
