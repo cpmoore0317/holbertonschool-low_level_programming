94653313d9ebdb05841a40a3a713e8f0a4f4a506
“cpmoore0317” <kingpkayy@gmail.com>
2023-08-19 11:49:27 -0700

    Creating a description for linear_search and creating a README.md file

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -1,5 +1,15 @@
 #include "search_algos.h"
 
+/*
+ * linear_search - searches for a value in an array of integers using the
+ * Linear search algorithm
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in array
+ * @value: value to search for
+ *
+ * Return: Value, -1 if failed
+ */
+
 int linear_search(int *array, size_t size, int value)
 {
 	if (array == NULL)
