a1a81e0ff06a01b840ca721a48b8b55418e98eba
“cpmoore0317” <kingpkayy@gmail.com>
2023-08-23 08:34:50 -0700

    Fixing declaration for binary search and adding time complexity

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -12,7 +12,7 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-	size_t left = 0, right = size - 1, mid;
+	size_t left = 0, right = size - 1, mid, i;
 
 	if (array == NULL)
 		return (-1);
@@ -22,7 +22,7 @@ int binary_search(int *array, size_t size, int value)
 		mid = left + (right - left) / 2;
 
 		printf("Searching in array: ");
-		for (size_t i = left; i <= right; i++)
+		for (i = left; i <= right; i++)
 		{
 			printf("%d", array[i]);
 			if (i < right)
